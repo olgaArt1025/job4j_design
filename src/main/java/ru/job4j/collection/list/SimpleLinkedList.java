e49e7d52795bacb010b07b6e08a075aa2702581a
Olga_ <olga_art1025@mail.ru>
2021-12-01 22:50:31 +0500

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,86 @@
+package ru.job4j.collection.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> first;
+    private Node<E> last;
+
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(last, value, null);
+        if (last == null) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+        size++;
+        modCount++;
+    }
+
+
+    @Override
+    public E get(int index) {
+        Node<E> node = first;
+        int i = 0;
+        while (i < checkIndex(index)) {
+            node = node.next;
+            i++;
+        }
+        return node.item;
+    }
+
+    private int checkIndex(int index) {
+        if ((index < 0) || (index >= size)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return index;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            Node<E> node = first;
+            private int point = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> lastReturned = node;
+                node = node.next;
+                point++;
+                return lastReturned.item;
+            }
+        };
+    }
+}
+
