da22e07e945f2396a6d7d1fc4af344f32f8b7e0c
Olga_ <olga_art1025@mail.ru>
2021-12-08 20:42:21 +0500

    hashCode и equals

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.hash;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,38 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+
+    }
+
+    public static void main(String[] args) {
+        User userOne = new User("Anna", 1, new GregorianCalendar(1998, Calendar.JULY, 23));
+        User userTwo = new User("Anna", 1, new GregorianCalendar(1998, Calendar.JULY, 23));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(userOne, new Object());
+        map.put(userTwo, new Object());
+
+        System.out.println("Какие у них hashCode?");
+        System.out.println("userOne hashcode: " + userOne.hashCode());
+        System.out.println("userTwo hashcode: " + userTwo.hashCode());
+
+        map.forEach((key, value) -> System.out.println(key + " : " + value));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
