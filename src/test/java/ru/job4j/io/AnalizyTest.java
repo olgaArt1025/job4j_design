d84bbe8e7d9995a14fbb810f5beba709081697c4
Olga_ <olga_art1025@mail.ru>
2021-12-18 19:42:53 +0500

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,13 +1,19 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+import java.io.*;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
 
     @Test
     public void serverIsUnavailableFirstTime() {
@@ -33,6 +39,26 @@ public class AnalizyTest {
             assertThat(read.readLine(), is("10:57:01;11:02:02;"));
         } catch (Exception e) {
             e.printStackTrace(System.err);
+        }
+    }
+
+    @Test
+    public void serverIsUnavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("200 11:02:02");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+            assertThat(rsl.toString(), is("10:57:01;11:02:02;"));
         }
     }
 }
