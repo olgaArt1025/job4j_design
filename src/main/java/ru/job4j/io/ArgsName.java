0733ec247f6838887a98fe8fb019cde6216ec766
Olga_ <olga_art1025@mail.ru>
2021-12-21 00:20:00 +0500

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        String rsl = values.get(key);
+        if (rsl == null) {
+            throw new IllegalArgumentException();
+        }
+        return rsl;
+    }
+
+    private void parse(String[] args) {
+        Arrays.stream(args)
+                .forEach(line -> {
+                    line = line.replace("-", "");
+                    String[] words = line.split("=");
+                    if (words.length != 2) {
+                        throw new IllegalArgumentException();
+                    }
+                    values.put(words[0], words[1]);
+                });
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
