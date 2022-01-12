d7a0b8df41c9123a7fe28de0f66e5af83389d57a
Olga_ <olga_art1025@mail.ru>
2022-01-12 23:29:48 +0500

    0. Join [#6862]перенос

--- sql/departments.sql
+++ sql/departments.sql
@@ -9,26 +9,46 @@ create table emploers (
  	departments_id int references departments(id)
 );
 
-insert into departments (name) values ('Администрация'),('Продажи'),('Программисты'), ('Закуп');
-insert into emploers (name, departments_id) values ('Петя', 1),('Таня', 1),('Слава', 2), ('Миша', 2), ('Андрей', 3),('Паша', 3); 
+insert into departments (name) values
+('Администрация'),('Продажи'),('Программисты'), ('Закуп');
 
-select * from departments d left join emploers e on d.id=e.departments_id;
+insert into emploers (name, departments_id) values
+('Петя', 1),('Таня', 1),('Слава', 2), ('Миша', 2), ('Андрей', 3),('Паша', 3);
 
-select * from departments d right join emploers e on d.id = e.departments_id;
+select * from departments d
+left join emploers e
+on d.id=e.departments_id;
 
-select * from emploers e left join departments d on d.id = e.departments_id;
+select * from departments d
+right join emploers e
+on d.id = e.departments_id;
 
-select * from emploers e right join departments  d on d.id = e.departments_id;
+select * from emploers e
+left join departments d
+on d.id = e.departments_id;
 
-select * from departments d full join emploers e on d.id = e.departments_id;
+select * from emploers e
+right join departments  d
+on d.id = e.departments_id;
 
-select * from departments d cross join emploers e;
+select * from departments d
+full join emploers e
+on d.id = e.departments_id;
 
-select * from departments d left join emploers e on d.id = e.departments_id where departments_id is null;
+select * from departments d
+cross join emploers e;
 
-select d.name, e.name from emploers  e right join departments  d on e.departments_id = d.id;
+select * from departments d
+left join emploers e
+on d.id = e.departments_id
+where departments_id is null;
 
-select d.name, e.name from departments  d left join emploers  e on e.departments_id = d.id;
+select d.name, e.name from emploers  e
+right join departments  d
+on e.departments_id = d.id;
+
+select d.name, e.name from departments  d
+left join emploers  e on e.departments_id = d.id;
 
  create table  teens (
  id serial primary key,
@@ -36,7 +56,11 @@ select d.name, e.name from departments  d left join emploers  e on e.departments
 	gender varchar(10)
 );
 
-insert into teens (name, gender) values ('Sergey', 'male'), ('Irina', 'female'), ('Ivan', 'male'), ('Nikita','male'), ('Olga', 'female');
+insert into teens (name, gender) values
+('Sergey', 'male'), ('Irina', 'female'), ('Ivan', 'male'),
+('Nikita','male'), ('Olga', 'female');
 
-select t1.name as female, t2.name as male from  teens t1 cross  join  teens t2 where t1.gender!=t2.gender and t1.gender = 'female'; 
+select t1.name as female, t2.name as male
+from  teens t1 cross  join  teens t2
+where t1.gender!=t2.gender and t1.gender = 'female';
  
